694ae3e954e50d6e517f5f938b0bdc8eafe5d11f
Maksym <m.chukhrai@gmail.com>
2024-09-21 15:56:20 +0300

    add Readme

--- src/stories/Input.stories.jsx
+++ src/stories/Input.stories.jsx
@@ -1,98 +1,92 @@
+
 import React from 'react';
-import classNames from 'classnames';
-import InputGroup from '../components/InputGroup/InputGroup';
-import InputLabel from '../components/InputLabel/InputLabel';
 import InputText from '../components/InputText/InputText';
+import InputLabel from '../components/InputLabel/InputLabel';
+import InputGroup from '../components/InputGroup/InputGroup';
 import InputAnnotation from '../components/InputAnnotation/InputAnnotation';
 
-import IconBefore from '../assets/icons/icon-before.png';
-import IconAfter from '../assets/icons/icon-after.png';
-
 export default {
-  title: 'Input Components',
+  title: 'Components/Input',
   component: InputGroup,
   argTypes: {
     labelSize: {
-      control: { type: 'select', options: ['sm', 'md', 'lg'] },
+      control: { type: 'select', options: ['small', 'medium', 'large'] },
     },
     state: {
-      control: { type: 'select', options: ['default', 'success', 'error'] },
+      control: { type: 'select', options: ['default', 'hover', 'focus', 'disabled', 'error'] },
     },
     labelPosition: {
-      control: { type: 'radio', options: ['top', 'side'] },
+      control: { type: 'radio', options: ['top', 'left'] },
     },
-    helperText: { control: 'boolean' },
-    label: { control: 'boolean' },
-    info: { control: 'boolean' },
-    size: {
-      control: { type: 'select', options: ['sm', 'md', 'lg'] },
+    showBorder: {
+      control: 'boolean',
+    },
+    showHelperText: {
+      control: 'boolean',
+    },
+    showLabel: {
+      control: 'boolean',
+    },
+    showInfoIcon: {
+      control: 'boolean',
+    },
+    inputSize: {
+      control: { type: 'select', options: ['small', 'medium', 'large'] },
     },
     alignment: {
-      control: { type: 'radio', options: ['left', 'center', 'right'] },
+      control: { type: 'select', options: ['left', 'center', 'right'] },
     },
-    iconAfter: { control: 'boolean' },
-    iconBefore: { control: 'boolean' },
-    readonly: { control: 'boolean' },
+    showIconBefore: {
+      control: 'boolean',
+    },
+    showIconAfter: {
+      control: 'boolean',
+    },
+    showInputTextIcon: {
+      control: 'boolean',
+    },
   },
 };
 
 const Template = (args) => {
-  const [value, setValue] = React.useState('');
-  const [showIconBefore, setShowIconBefore] = React.useState(args.iconBefore);
-  const [showIconAfter, setShowIconAfter] = React.useState(args.iconAfter);
-
-  React.useEffect(() => {
-    setShowIconBefore(args.iconBefore);
-    setShowIconAfter(args.iconAfter);
-  }, [args.iconBefore, args.iconAfter]);
-
-  const inputClass = classNames({
-    [`state-${args.state}`]: args.state,
-    [`size-${args.size}`]: args.size,
-    [`align-${args.alignment}`]: args.alignment,
-  });
-
-  const labelClass = classNames({
-    [`size-${args.labelSize}`]: args.labelSize,
-  });
+  const [inputValue, setInputValue] = React.useState('');
 
   return (
-    <InputGroup
-      style={{ flexDirection: args.labelPosition === 'side' ? 'row' : 'column' }}
-    >
-      {args.label && (
+    <InputGroup style={{ width: '300px' }}>
+      {args.showLabel && (
         <InputLabel
           label="Email"
-          showInfoIcon={args.info}
-          className={labelClass}
+          showInfoIcon={args.showInfoIcon}
+          className={args.labelSize}
         />
       )}
       <InputText
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className={inputClass}
-        readOnly={args.readonly}
-        iconBefore={showIconBefore ? <IconBefore /> : null}
-        iconAfter={showIconAfter ? <IconAfter /> : null}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Input..."
+        iconBeforeLeft={args.showIconBefore ? undefined : null}
+        iconAfterLeft={args.showIconAfter ? undefined : null}
+        iconAfterRight={args.showInputTextIcon ? undefined : null}
+        className={`${args.state} ${args.inputSize} ${args.alignment}`}
+        style={{ border: args.showBorder ? undefined : 'none' }}
       />
-      {args.helperText && (
-        <InputAnnotation text="This is a hint text to help the user." />
-      )}
+      {args.showHelperText && <InputAnnotation text="This is a hint text to help user." />}
     </InputGroup>
   );
 };
 
 export const Playground = Template.bind({});
 Playground.args = {
-  labelSize: 'md',
+  labelSize: 'medium',
   state: 'default',
   labelPosition: 'top',
-  helperText: true,
-  label: true,
-  info: true,
-  size: 'md',
+  showBorder: true,
+  showHelperText: true,
+  showLabel: true,
+  showInfoIcon: true,
+  inputSize: 'medium',
   alignment: 'left',
-  iconAfter: true,
-  iconBefore: true,
-  readonly: false,
+  showIconBefore: true,
+  showIconAfter: true,
+  showInputTextIcon: true,
 };
